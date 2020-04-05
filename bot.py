13f46a512971fa838643d21adb3285b54132a3e4
Ryan Davila <ryan.m.davila@gmail.com>
2020-04-05 00:56:01 -0400

    Add basic board representation and move logic

--- bot.py
+++ bot.py
@@ -5,35 +5,37 @@ from typing import List, Dict
 
 from game import Move, GameState, move
 
+
 class AI:
     """Representation of bot's state"""
+
     scores: Dict[GameState, float]
     current_game_states = []
 
     def choose_move(self, state: GameState) -> Move:
-        current_game_states.append(state)
+        self.current_game_states.append(state)
         moves = state.possible_moves()
         next_states = []
         for move in moves:
-            next_states.add(move(state, move))
+            next_states.append(move(state, move))
 
-        probabilities = np.array([logistic(self.scores[state]) for state in next_states])
+        probabilities = np.array(
+            [logistic(self.scores[state]) for state in next_states]
+        )
         probabilities /= np.sum(probabilities)
         return np.random.choice(moves, p=probabilities)
 
-    def game_won():
-        for state in current_game_states:
-            scores[state] += 1
-        current_game_states = []
+    def game_won(self):
+        for state in self.current_game_states:
+            self.scores[state] += 1
+        self.current_game_states = []
 
-    def game_loss():
-        for state in current_game_states:
-            scores[state] -= 1
-        current_game_states = []
+    def game_loss(self):
+        for state in self.current_game_states:
+            self.scores[state] -= 1
+        self.current_game_states = []
 
-    def game_draw():
-        for state in current_game_states:
-            scores[state] -= 0.1
-        current_game_states = []
-
-
+    def game_draw(self):
+        for state in self.current_game_states:
+            self.scores[state] -= 0.1
+        self.current_game_states = []
--- game.py
+++ game.py
@@ -2,29 +2,83 @@ import numpy as np
 
 
 class Move:
-    pass
+    current_position: ()
+    new_position: ()
+    player: str
+
+    def __init__(self, current_position, new_position, player):
+        self.current_position = current_position
+        self.new_position = new_position
+        self.player = player
 
 
 class GameState:
-    board: [[]]
+    """Representation of the game board and current state"""
+
+    starting_positions = set(
+        (0, 0),
+        (0, 2),
+        (0, 4),
+        (0, 6),
+        (1, 1),
+        (1, 3),
+        (1, 5),
+        (1, 7),
+        (7, 0),
+        (7, 2),
+        (7, 4),
+        (7, 6),
+        (6, 1),
+        (6, 3),
+        (6, 5),
+        (6, 7),
+    )
+    board: {}
     board_size: int
     p1_turn: bool
 
-    def __init__(self, board_size: int=8):
-      for i in range(board_size):
-        for j in range(board / 2):
-            board[i][j] = 0
+    def __init__(self, board_size: int = 8):
+        self.board_size = board_size
+        for row in range(board_size):
+            for column in range(board_size):
+                if (row, column) in self.starting_positions:
+                    self.board[(row, column)] = 1
+                else:
+                    self.board[(row, column)] = 0
+        self.p1_turn = True
 
-        
-
-    def __eq__(self):
+    def __eq__(self, obj):
         pass
 
     def __hash__(self):
         pass
 
     def possible_moves(self) -> Iterable[Move]:
-        pass
+        possible_moves = []
+        for point in self.board.values():
+            for possible_move in self._get_diagonal_positions(point):
+                if is_valid_move(point, possible_move, self.board):
+                    possible_moves.append(
+                        Move(point, possible_move, "p1" if self.p1_turn else "p2")
+                    )
+        return possible_moves
+
+    def _get_diagonal_positions(self, position):
+        return set(
+            (position[0] + 1, position[1] + 1),
+            (position[0] - 1, position[1] - 1),
+            (position[0] + 1, position[1] - 1),
+            (position[0] - 1, position[1] + 1),
+        )
+
+
+def is_valid_move(current_position, new_position, board) -> bool:
+    if abs(current_position[0] - new_position[0]) == abs(
+        current_position[1] - new_position[1]
+    ):
+        if board[new_position] != 1:
+            return True
+    return False
 
 
 def move(state: GameState, move: Move) -> GameState:
